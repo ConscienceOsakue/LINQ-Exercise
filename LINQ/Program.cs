63a5cb9005b37cd2ff4921c1ff3e2a689b2fecac
Conscience Osakue <consciencenosa@gmail.com>
2023-12-07 17:58:31 -0600

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> shenGame = new List<string>()
+        {
+            "ShenPop Quiz",
+            "ShenMind Game",
+            "ShenLove Text",
+            "ShenCrazy Creative",
+
+        };
+
+            shenGame.Add("ShenMatch Up");
+            shenGame.Add("ShenMake Codes");
+
+            var orderShenGames = shenGame.OrderBy(shen => shen.Length);
+
+            Console.WriteLine("Below is the list of Shen Games Sorted by Lenght.");
+            Console.WriteLine();
+
+            foreach (var shen in orderShenGames)
+            {
+               
+                
+                Console.WriteLine(shen);
+            }
         }
     }
 }
